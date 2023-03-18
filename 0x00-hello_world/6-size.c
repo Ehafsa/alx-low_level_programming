e2761abca8ca72f38decadd68d9285310da81276
Ehafsa <hafsa.elhafsa98@gmail.com>
2023-03-18 09:52:31 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:%d byte(s)", sizead(char));
+printf("Size of an int:%d byte(s)", sizead(int));
+printf("Size of a long int:%d byte(s)", sizead(long int));
+printf("Size of a long long int:%d byte(s)", sizead(long long int));
+printf("Size of a float:%d byte(s)", sizead(float));
+return (0);
+}
